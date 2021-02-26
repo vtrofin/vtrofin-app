93fb0f63edef43991030774d72bce144d6c9dfec
Victor Trofin <victor_trofin@yahoo.com>
2021-02-26 09:47:06 +0900

    Initial Commit

---
+++ vtrofin-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vtrofin-app
+//
+//  Created by ship-dev on 2021/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vtrofin-app/vtrofin_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  vtrofin_appApp.swift
+//  vtrofin-app
+//
+//  Created by ship-dev on 2021/02/26.
+//
+
+import SwiftUI
+
+@main
+struct vtrofin_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
